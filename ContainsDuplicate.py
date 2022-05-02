2ff3448aa8911dcd0587c9ab05a30b7add802372
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2022-05-02 15:32:26 +0600

    Contains duplicate

---
+++ ContainsDuplicate.py
@@ -0,0 +1,39 @@
+
+from turtle import rt
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+            
+        return False
+    
+    def containsDuplicate2(self, nums: List[int]) -> bool:
+        nums.sort()
+        for index in range(len(nums)-1):
+            if nums[index]==nums[index+1]:
+                return True
+        
+        return False
+            
+
+            
+        return False
+
+nums = [1,2,3,1]
+sln =  Solution()
+print(sln.containsDuplicate2(nums))
+nums = [1,2,3,3]
+print(sln.containsDuplicate2(nums))
+nums = [1,2,3,1]
+print(sln.containsDuplicate(nums))
+nums = [1,2,3]
+print(sln.containsDuplicate(nums))
+
+
